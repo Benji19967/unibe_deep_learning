3e1e2cf6ff98d41e97baf22d561f23ac7396dba6
Benjamin Labrecque <labrecque.benji@gmail.com>
2026-03-15 15:56:20 +0100

    feat: add NN classifier

--- makefile
+++ makefile
@@ -1,4 +1,17 @@
-include ../build_tools/poetry.mk
+DATA_DIR = data/raw
+URL =  https://download.microsoft.com/download/3/e/1/3e1c3f21-ecdb-4869-8368-6deba77b919f/kagglecatsanddogs_5340.zip
+FILE = $(DATA_DIR)/kagglecatsanddogs_5340.zip
+
+all: download unzip
+
+$(DATA_DIR):
+	mkdir -p $(DATA_DIR)
+
+download: $(DATA_DIR)
+	wget -O $(FILE) $(URL)
+
+unzip: download
+	unzip -o $(FILE) -d $(DATA_DIR)
 
 test:
 	pytest tests/ -vv -s
--- src/main.py
+++ src/main.py
@@ -1,5 +1,16 @@
+from data_loader import get_train_and_test_datasets, load_images
+from models.nn_classifier import NNClassifierVectorized
+
+
 def main():
-    pass
+    dog_images, cat_images = load_images()
+    x_train, x_test, y_train, y_test = get_train_and_test_datasets(
+        dog_images, cat_images
+    )
+    classifier = NNClassifierVectorized()
+    classifier.train(x_train, y_train)
+    predicted_label, min_distance, closest_image = classifier.predict(x_test[0])
+    print(predicted_label)
 
 
 if __name__ == "__main__":
